8f1205e47251792cf6ef2e772afe6580a61f59af
Amelie Boudier <amelie.boudier01@gmail.com>
2019-01-24 15:29:50 +0100

    added portfolio page and pointer on resume

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :resume]
+  skip_before_action :authenticate_user!, only: [:index, :resume, :portfolio]
 
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
   get '/resume', to: 'pages#resume'
+  get '/portfolio', to: 'pages#portfolio'
 end
